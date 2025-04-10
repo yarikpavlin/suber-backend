6400be7bf5b30e7650fbbd015ad3b9e48788a7fb
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2025-04-10 22:50:17 +0300

    fix: issue with 403 status code
    - feat: handle no user data

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,12 +5,22 @@ import { MockUserRepository } from "../repositories/mockUser";
 const userRepo = new MockUserRepository();
 
 const getUsers = async (req: Request, res: Response) => {
-    res.send(200).json(await userRepo.readAll())
+    const users = await userRepo.readAll();
+    if (users.length === 0) {
+        res.status(404).json({ error: 'No users found' });
+    } else {
+        res.status(200).json(users);
+    }
 }
 
 const getUserById = async (req: Request, res: Response) => {
     const id = req.params.id as UUID;
-    res.send(200).json(await userRepo.read(id))
+    const user = await userRepo.read(id);
+    if (!user) {
+        res.status(404).json({ error: 'User not found' });
+    } else {
+        res.status(200).json(user);
+    }
 }
 
 export {
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import cors from "cors";
 import express from "express";
 import userRoutes from './routes/users';
 
 const app = express();
-const PORT = 5000;
+const PORT = 3030;
 
-app.use(cors());
 app.use(express.json());
 app.use('/users', userRoutes);
 
