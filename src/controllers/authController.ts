3893f65723224f493e8b72099b210440cf8d8fe6
Yaroslav Pavlinskiy <yarbezl@gmail.com>
2025-04-11 03:34:08 +0300

    feat: add dotenv support

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -8,7 +8,7 @@ const userRepo = new MockUserRepository();
 const signup = async (req: Request, res: Response) => {
     try {
         const user = await userRepo.create(req.body);
-        const token = jwt.sign({ id: user.id }, 'secret');
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string);
         res.status(201).header('Authorization', `Bearer ${token}`).json({id: user.id, email: user.email});
     } catch (error) {
         if (error instanceof ApiError) {
@@ -28,7 +28,7 @@ const login = async (req: Request, res: Response) => {
     if (!bcrypt.compareSync(password, user.password)) {
         return res.status(401).json({ message: 'Invalid credentials' });
     }
-    const token = jwt.sign({ id: user.id }, 'secret');
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string);
     res.status(200).header('Authorization', `Bearer ${token}`).json({id: user.id, email: user.email});
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
-const PORT = 3030;
 
 app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/auth', authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
